4c6f151ee0c13f0030c14e85b89d7fbfc1e23aa5
rokielal <manoranjangupta94@gmail.com>
2024-03-18 02:59:13 +0530

    Started Youtube clone

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": "b03e2184ecmshb79bd6f751aae2ap18b15cjsnb21841c1466b",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
