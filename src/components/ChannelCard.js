2e7bdb232b52a1f5a1ea026a6e10d74dfefa239f
rokielal <manoranjangupta94@gmail.com>
2024-03-18 16:41:30 +0530

    added search functionality add deployed

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Box, Typography, CardMedia, CardContent } from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constaints";
 
-const ChannelCard = () => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
   return (
-    <div>ChannelCard</div>
-  )
-}
+    <Box
+      sx={{
+        boxShadow: "none",
+        borderRadius: "20px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: { xs: "356px", md: "320px" },
+        height: "326px",
+        margin: "auto",
+        marginTop: marginTop,
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            textAlign: "center",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircleIcon sx={{ fontSize: 14, color: "gray", ml: "5px" }} />
+          </Typography>
 
-export default ChannelCard
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}{" "}
+              Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
+};
+
+export default ChannelCard;
